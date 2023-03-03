5d1dd9cbf24f85b444b68a10da6b7b76f4eff9b8
Alexandr Ainullin <aainullin@gmail.com>
2023-03-03 22:09:40 +0300

    1

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,20 +1,18 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
+from rest_framework import filters, viewsets
 from rest_framework.decorators import action, api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
-from rest_framework import viewsets, filters
+from rest_framework_simplejwt.tokens import RefreshToken
 
-from .serializers import (UserSerializer,
-                          AdminSerializer,
-                          SignUpSerializer,
-                          TokenSerializer)
 from .models import User
 from .permissions import IsAdmin
+from .serializers import (AdminSerializer, SignUpSerializer, TokenSerializer,
+                          UserSerializer)
 
 
 class UserViewSet(viewsets.ModelViewSet):
